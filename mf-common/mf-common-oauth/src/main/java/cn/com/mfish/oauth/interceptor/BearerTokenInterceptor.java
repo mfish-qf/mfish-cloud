7ea336eb657f37dccbdf993fa5e75daf06cb7e5f
qiufeng <qiufeng9862@qq.com>
2021-12-13 10:47:20 +0800

    增加Feign请求token中继

--- mf-common/mf-common-oauth/src/main/java/cn/com/mfish/oauth/interceptor/BearerTokenInterceptor.java
+++ mf-common/mf-common-oauth/src/main/java/cn/com/mfish/oauth/interceptor/BearerTokenInterceptor.java
@@ -5,6 +5,7 @@ import cn.com.mfish.common.core.utils.StringUtils;
 import cn.com.mfish.oauth.common.Utils;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
+import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -15,6 +16,7 @@ import java.util.Objects;
  * @description：Feign请求令牌中继，防止令牌丢失
  * @date ：2021/12/13 10:19
  */
+@Component
 public class BearerTokenInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate requestTemplate) {
