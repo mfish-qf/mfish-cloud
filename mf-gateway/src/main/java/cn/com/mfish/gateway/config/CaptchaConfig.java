48ccb3f935a72d4d0ae20f09654f3cb657eac8da
qiufeng <qiufeng9862@qq.com>
2021-12-21 18:37:30 +0800

    页面增加验证码

--- mf-gateway/src/main/java/cn/com/mfish/gateway/config/CaptchaConfig.java
+++ mf-gateway/src/main/java/cn/com/mfish/gateway/config/CaptchaConfig.java
@@ -31,18 +31,14 @@ public class CaptchaConfig {
     public DefaultKaptcha mathCaptchaProducer() {
         DefaultKaptcha defaultKaptcha = new DefaultKaptcha();
         Properties properties = createDefaultProperties();
-        // 边框颜色 默认为Color.BLACK
-        properties.setProperty(KAPTCHA_BORDER_COLOR, "105,179,90");
+
         // 验证码文本字符颜色 默认为Color.BLACK
         properties.setProperty(KAPTCHA_TEXTPRODUCER_FONT_COLOR, "blue");
-        // 验证码文本字符大小 默认为40
-        properties.setProperty(KAPTCHA_TEXTPRODUCER_FONT_SIZE, "35");
         // 缓存key
         properties.setProperty(KAPTCHA_SESSION_CONFIG_KEY, "mathCaptchaCode");
         properties.setProperty(KAPTCHA_TEXTPRODUCER_IMPL, "cn.com.mfish.gateway.config.MathCaptchaCreate");
         // 验证码文本字符间距 默认为2
         properties.setProperty(KAPTCHA_TEXTPRODUCER_CHAR_SPACE, "3");
-        // 验证码文本字体样式 默认为new Font("Arial", 1, fontSize), new Font("Courier", 1, fontSize)
         // 验证码噪点颜色 默认为Color.BLACK
         properties.setProperty(KAPTCHA_NOISE_COLOR, "white");
         // 干扰实现类
@@ -56,18 +52,20 @@ public class CaptchaConfig {
         Properties properties = new Properties();
         // 是否有边框 默认为true 我们可以自己设置yes，no
         properties.setProperty(KAPTCHA_BORDER, "yes");
+        // 边框颜色 默认为Color.BLACK
+        properties.setProperty(KAPTCHA_BORDER_COLOR, "85,85,85");
         // 验证码文本字符颜色 默认为Color.BLACK
-        properties.setProperty(KAPTCHA_TEXTPRODUCER_FONT_COLOR, "black");
+        properties.setProperty(KAPTCHA_TEXTPRODUCER_FONT_COLOR, "85,85,85");
         // 验证码图片宽度 默认为200
-        properties.setProperty(KAPTCHA_IMAGE_WIDTH, "160");
+        properties.setProperty(KAPTCHA_IMAGE_WIDTH, "200");
         // 验证码图片高度 默认为50
-        properties.setProperty(KAPTCHA_IMAGE_HEIGHT, "60");
+        properties.setProperty(KAPTCHA_IMAGE_HEIGHT, "50");
         // 验证码文本字符大小 默认为40
-        properties.setProperty(KAPTCHA_TEXTPRODUCER_FONT_SIZE, "38");
+        properties.setProperty(KAPTCHA_TEXTPRODUCER_FONT_SIZE, "40");
         // 缓存key
         properties.setProperty(KAPTCHA_SESSION_CONFIG_KEY, "captchaCode");
         // 验证码文本字符长度 默认为5
-        properties.setProperty(KAPTCHA_TEXTPRODUCER_CHAR_LENGTH, "4");
+        properties.setProperty(KAPTCHA_TEXTPRODUCER_CHAR_LENGTH, "5");
         // 验证码文本字体样式 默认为new Font("Arial", 1, fontSize), new Font("Courier", 1, fontSize)
         properties.setProperty(KAPTCHA_TEXTPRODUCER_FONT_NAMES, "Arial,Courier");
         // 图片样式 水纹com.google.code.kaptcha.impl.WaterRipple 鱼眼com.google.code.kaptcha.impl.FishEyeGimpy 阴影com.google.code.kaptcha.impl.ShadowGimpy
